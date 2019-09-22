d6a6c304eca1d7ae0cb5b41fcce461e29b5641a6
Carl Ivanov <bdgtw502@mail.ru>
2019-09-22 06:44:31 +0300

    Compile and removing and runnig fix for windows

--- dm_compile_run.sh
+++ dm_compile_run.sh
@@ -20,17 +20,17 @@ timeout="$HOME/timeout --just-kill --no-info-on-success --detect-hangups -h 10 -
 file=$1
 
 if [[ $2 == "run" ]]; then
-	DreamDaemon $file.dmb -invisible -ultrasafe 2>&1 | tail -n +4 | head -c 512
+	dreamdaemon.exe $file.dmb -invisible -ultrasafe 2>&1 | tail -n +4 | head -c 512
 	exit 0
 fi
 
 cd dm/
 
-export BYOND_SYSTEM=/home/nyx/byond/use
-export PATH=/home/nyx/byond/use/bin:$PATH
-export LD_LIBRARY_PATH=/home/nyx/byond/use/bin:$LD_LIBRARY_PATH
+# export BYOND_SYSTEM=/home/nyx/byond/use
+# export PATH=/home/nyx/byond/use/bin:$PATH
+# export LD_LIBRARY_PATH=/home/nyx/byond/use/bin:$LD_LIBRARY_PATH
 
-output=$($timeout DreamMaker $file.dme 2>&1)
+output=$($timeout dm.exe $file.dme 2>&1)
 return=$?
 
 if [[ $return != 0 ]]; then
